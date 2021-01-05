c576a3f5834a99c3823dbb0e0be314e65fc559a4
Turston Louw <53752666+turstoncodes@users.noreply.github.com>
2021-01-05 19:03:52 +0200

    Update blogscraping.py

--- blogscraping.py
+++ blogscraping.py
@@ -1,21 +1,21 @@
- 1 import requests
- 2 from bs4 import BeautifulSoup
- 3 from csv import writer
- 4 
- 5 response = requests.get('http://codedemos.com/sampleblog/')
- 6 
- 7 soup = BeautifulSoup(response.text, 'html.parser')
- 8 
- 9 posts = soup.find_all(class_='post-preview')
- 10 
- 11 with open('posts.csv', 'w') as csv_file:
- 12 csv_writer = writer(csv_file)
- 13 headers = ['Title', 'Link', 'Date']
- 14 csv_writer.writerow(headers)
- 15 
- 16 for post in posts:
- 17     title = post.find(class_='post-title').get_text().replace('\n', '')
- 18     link = post.find('a')['href']
- 19     date = post.select('.post-date')[0].get_text()
- 20     csv_writer.writerow([title, link, date])
- 21
+  import requests
+  from bs4 import BeautifulSoup
+  from csv import writer
+  
+  response = requests.get('http://codedemos.com/sampleblog/')
+  
+  soup = BeautifulSoup(response.text, 'html.parser')
+ 
+  posts = soup.find_all(class_='post-preview')
+
+  with open('posts.csv', 'w') as csv_file:
+  csv_writer = writer(csv_file)
+  headers = ['Title', 'Link', 'Date']
+  csv_writer.writerow(headers)
+  
+  for post in posts:
+      title = post.find(class_='post-title').get_text().replace('\n', '')
+     link = post.find('a')['href']
+     date = post.select('.post-date')[0].get_text()
+     csv_writer.writerow([title, link, date])
+ 
